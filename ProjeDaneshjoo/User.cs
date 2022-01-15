0ae984fd63a6be9b4287d0b8cd8a3a78a32bdea8
MohamadHjlo <hjiloomohamad@gmail.com>
2022-01-16 00:55:13 +0330

    first commit

---
+++ ProjeDaneshjoo/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjeDaneshjoo
+{
+    internal class User
+    {
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        
+    }
+}
